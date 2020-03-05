fc054a6b8760afac7b532f990a095521db18ea0e
Matteo Antunez <47684357+matteoantunez@users.noreply.github.com>
2020-03-04 19:35:32 -0600

    Begin Implementation of AJAX

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+window.addEventListener('load', (event) =>{
+    if (localStorage.getItem('pOdometer')){
+        document.querySelector('#pOdometer').value = localStorage.getItem('pOdometer')
+    }
+    if (localStorage.getItem('cOdometer')){
+        document.querySelector('#cOdometer').value = localStorage.getItem('cOdometer')
+    }
+    if (localStorage.getItem('gasFilled')){
+        document.querySelector('#gasFilled').value = localStorage.getItem('gasFilled')
+    }
+})
+
 document.querySelector('#calc').addEventListener('click', () => {
     let previous = parseInt(document.querySelector('#pOdometer').value)
     let cur = parseInt(document.querySelector('#cOdometer').value)
@@ -6,4 +18,8 @@ document.querySelector('#calc').addEventListener('click', () => {
 
     mpg = (cur - previous) / tanks
     document.querySelector('#result').innerHTML = mpg + "MPG"
+
+    localStorage.setItem('pOdometer', previous)
+    localStorage.setItem('cOdometer', cur)
+    localStorage.setItem('gasFilled', tanks)
 })
