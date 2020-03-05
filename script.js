e00c40ce688978fb67e53f72d85e786915c05f6f
Matteo Antunez <47684357+matteoantunez@users.noreply.github.com>
2020-03-04 19:10:46 -0600

    Finished JavaScript formula for Calculations

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-document.querySelector('#calculate').addEventListener('click', function() {
+document.querySelector('#calc').addEventListener('click', () => {
     let previous = parseInt(document.querySelector('#pOdometer').value)
     let cur = parseInt(document.querySelector('#cOdometer').value)
     let tanks = parseInt(document.querySelector('#gasFilled').value)
     let mpg = 0
 
     mpg = (cur - previous) / tanks
-    document.querySelector('#result').innerHTML = mpg
+    document.querySelector('#result').innerHTML = mpg + "MPG"
 })
